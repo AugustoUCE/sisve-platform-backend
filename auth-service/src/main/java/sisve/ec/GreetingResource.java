5020a95c4e8ff7b11356e06f7d1dce113271edf7
AugustoUCE <aysalazar@uce.edu.ec>
2026-06-23 19:24:55 -0500

    QualityAssurance branch

--- auth-service/src/main/java/sisve/ec/GreetingResource.java
+++ auth-service/src/main/java/sisve/ec/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello rama DESARROLLO";
+        return "Hello rama QA";
     }
 }
